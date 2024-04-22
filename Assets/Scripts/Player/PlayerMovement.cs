53cbaf09174ec63c3203b7d0fc17a4c868efbf16
Albertengo <145947798+Albertengo@users.noreply.github.com>
2024-04-22 01:57:21 -0300

    Movimiento del player.
    Funciona pero capaz hay que arreglarlo un poco

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,23 +4,63 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] float speed = 10f;
+    //NOTA: por alguna razón a veces puede salir volando el player,a hay q ver cómo arreglarlo!!
+
+    [Header("Movimiento")]
+    public float rapidezDesplazamiento = 10.0f;
+    public float modificadorSprint; //esto igual tmb sirve si queremos q tenga sigilo, sólo q no va a correr. En vez de num positivo ponemos negativo o 0 y listo, funca!!!
+    
+    Vector3 direccion;
+    public Transform Orientation;
     [SerializeField] Rigidbody rb;
 
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
 
     void Update()
     {
-        float forwardMovement = Input.GetAxis("Vertical") * speed;
-        float horizontalMovement = Input.GetAxis("Horizontal") * speed;
+        //float forwardMovement = Input.GetAxis("Vertical") * speed;
+        //float horizontalMovement = Input.GetAxis("Horizontal") * speed;
 
-        forwardMovement *= Time.deltaTime;
-        horizontalMovement *= Time.deltaTime;
+        //forwardMovement *= Time.deltaTime;
+        //horizontalMovement *= Time.deltaTime;
 
-        transform.Translate(horizontalMovement, 0, forwardMovement);
+        //transform.Translate(horizontalMovement, 0, forwardMovement);
+        movimiento();
+        speedControl();
+    }
+
+    void movimiento()
+    {
+        float speedsprint = rapidezDesplazamiento; //para poder modificar la rapidez dentro del código sin alterar la default.
+
+        //movimiento normal.
+        float movimientoAdelanteAtras = Input.GetAxis("Vertical");
+        float movimientoCostados = Input.GetAxis("Horizontal");
+        direccion = Orientation.forward * movimientoAdelanteAtras + Orientation.right * movimientoCostados; //obtiene informacion de la direccion mediante el transform usado
+
+
+        //lógica para el sprint
+        bool correr = Input.GetKey(KeyCode.LeftShift); //mientras se presione shift, bool será true.
+        bool esta_corriendo = correr && movimientoAdelanteAtras > 0; // mientras correr sea true y se esté caminando en eje z, bool será true.
+        if (esta_corriendo) speedsprint *= modificadorSprint; //mientras bool sera true, la velocidad será modificada.
+
+        //desplaza el rigidbody hacia la direccion que se esté mirando con la velocidad configurada
+        rb.AddForce(direccion.normalized * speedsprint * 10f, ForceMode.Force); // *10f para hacerlo un poco más rápido
+    }
+
+    //para controlar que no se pase la velocidad.
+    void speedControl()
+    {
+        Vector3 flatVelocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z); //calcula la velocidad alcanzada
+
+        if (flatVelocity.magnitude > rapidezDesplazamiento) //si esa velocidad se llega a pasar de la default,
+        {
+            Vector3 LimitedVel = flatVelocity.normalized * rapidezDesplazamiento; //calcula la max velocidad que se puede alcanzar
+            rb.velocity = new Vector3(LimitedVel.x, rb.velocity.y, LimitedVel.z); //y la aplica
+        }
     }
 }
