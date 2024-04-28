6d6dcf2354583f228b9ae7898a8ccbc4c42784e6
Albertengo <145947798+Albertengo@users.noreply.github.com>
2024-04-27 22:21:38 -0300

    UI DE VIDA FUNCIONA!!!!!!
    les gooooo

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,14 +1,31 @@
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     public int maxHealth = 3;
     public int currentLives;
 
+    //public int health;
+    //public int numOfHearts;
+
+    public Image[] hearts;
+    public Sprite fullPlushie;
+    public Sprite emptyPlushie;
+
     private void Start()
     {
         currentLives = maxHealth;
+    }
+
+    private void Update()
+    {
+        if (currentLives > maxHealth)
+        {
+            currentLives = maxHealth;
+        }
+        ChequeoPlushies();
     }
 
     private void OnCollissionEnter(Collision collision)
@@ -21,7 +38,7 @@ public class PlayerHealth : MonoBehaviour
     }
 
   public  void TakeDamage()
-    {
+  {
         currentLives--;
 
         if (currentLives <= 0)
@@ -33,6 +50,22 @@ public class PlayerHealth : MonoBehaviour
         {
             Debug.Log("Lost a life. Lives left: " + currentLives);
       
+        }
+    }
+
+    public void ChequeoPlushies()
+    {
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < currentLives)
+                hearts[i].sprite = fullPlushie;
+            else
+                hearts[i].sprite = emptyPlushie;
+
+            if (i < maxHealth)
+                hearts[i].enabled = true;
+            else
+                hearts[i].enabled = false;
         }
     }
 }
