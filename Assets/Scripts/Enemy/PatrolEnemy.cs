d4709615bfbb136960010bb663c5a6aa54156d61
aylen1113 <aylenrociolopez31@gmail.com>
2024-08-25 20:46:22 -0300

    patrullaje

--- Assets/Scripts/Enemy/PatrolEnemy.cs
+++ Assets/Scripts/Enemy/PatrolEnemy.cs
@@ -7,33 +7,37 @@ public class PatrolEnemy : MonoBehaviour
 {
     [SerializeField] NavMeshAgent navMeshAgent;
     [SerializeField] private Transform[] waypoints;
-    [SerializeField] private float waitTime;
-    private int currentWaypoint;
-    private bool isWaiting;
+    [SerializeField] private float waitTime = 2f;
+    [SerializeField] private float stoppingDistance = 0.5f;  
+
+    private int currentWaypoint = 0;
+    private bool isWaiting = false;
 
     void Update()
     {
-        if (transform.position != waypoints[currentWaypoint].position)
+
+        if (Vector3.Distance(transform.position, waypoints[currentWaypoint].position) > stoppingDistance)
         {
-           // transform.position = Vector3.MoveTowards(transform.position, waypoints[currentWaypoint].position, speed * Time.deltaTime);ç
+            
             navMeshAgent.destination = waypoints[currentWaypoint].position;
         }
         else if (!isWaiting)
         {
+     
             StartCoroutine(Wait());
         }
     }
 
     IEnumerator Wait()
     {
-        isWaiting = true;
-        yield return new WaitForSeconds(waitTime);
-        currentWaypoint++;
+        isWaiting = true;  
+        yield return new WaitForSeconds(waitTime);  
+        currentWaypoint++;  
 
         if (currentWaypoint == waypoints.Length)
         {
-            currentWaypoint = 0;
+            currentWaypoint = 0; 
         }
-        isWaiting = false;
+        isWaiting = false; 
     }
 }
